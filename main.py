2687fcd9f1b63ffac01590c2b10f43f8b3e1187b
LenaRedkinale <lenochkar35@mail.ru>
2022-10-01 19:54:03 +0300

    Добавлены условия задачи

--- main.py
+++ main.py
@@ -1,56 +1,30 @@
-
-from datetime import date
-from datetime import datetime
+from unittest import main, TestCase
 
 
-class Employee:
-    def __init__(self, name, birthdate, age):
+class Employee():
+    def __init__(self, name, birthdate):
         self.name = name
         self.birthdate = birthdate
-        self.age = age
 
-    def set_name(self, name):
-        self.name = name
-        return name
+    def give_raise(self, amount=5000):
+        self.salary += amount
 
-    def set_birthdate(self, birthdate):
-        self.birthdate = birthdate
-        birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
-        return birthdate
 
-    def calc_age(self, birthdate):
-        today = date.today()
-        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-        return age
-
-import unittest
-
-class TestEmployee(unittest.TestCase):
+class TestEmployee(TestCase):
     def setUp(self):
-        print("Test start")
-        self.test_name = "Olga"
-        self.test_date = "1986-03-04"
-        self.test_age = "36"
+        self.employee = Employee('Olga', 1986-04-03)
 
-    def tearDown(self):
-        pass
-        print("Test stop")
+    def test_give_default_raise(self):
+        self.employee.give_raise()
+        self.assertEqual(8500, self.employee.salary)
 
+    def test_give_custom_raise(self):
+        self.employee.give_raise(500)
+        self.assertEqual(4000, self.employee.salary)
 
-    def test_set_name(self):
-        print("test_set_name")
-        self.assertEqual(self.test_name, "Olga")
-
-
-    def test_set_birthdate(self):
-        print("test_set_birthdate")
-        self.assertEqual(self.test_date, "1986-03-04")
-
-
-    def test_calc_age(self):
-        print("test_calc_age")
-        self.assertEqual(self.test_age, "36")
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
+
+
 
